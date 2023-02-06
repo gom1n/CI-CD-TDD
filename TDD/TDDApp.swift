5c5ee88c2c5933546d727092cd4ac4d25f88fd85
soha <sa4022@naver.com>
2023-02-06 17:03:26 +0900

    Initial Commit

---
+++ TDD/TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDApp.swift
+//  TDD
+//
+//  Created by 소하 on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
