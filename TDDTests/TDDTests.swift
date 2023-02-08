2e7971684380746769394e462d187f3703d311ad
soha <sa4022@naver.com>
2023-02-08 19:04:50 +0900

    초기 정답값 확인용 테스트 케이스 추가

--- TDDTests/TDDTests.swift
+++ TDDTests/TDDTests.swift
@@ -13,13 +13,21 @@ final class TDDTests: XCTestCase {
     
     
     override func setUpWithError() throws {
+        try super.setUpWithError()
         bullAndCows = BullsandCows()
-        super.setUp()
     }
 
     override func tearDownWithError() throws {
         bullAndCows = nil
-        super.tearDown()
+        try super.tearDownWithError()
+    }
+    
+    // BullsandCows 생성 시 초기 정답 값 여부 확인
+    func testAnswerNotNil() {
+        // given, when
+        let answer = bullAndCows.getAnswer()
+        // then
+        XCTAssertNotEqual(answer.count, 0)
     }
     
     // SSS일 경우
@@ -30,7 +38,7 @@ final class TDDTests: XCTestCase {
         // when, 테스트 코드 실행
         let result = bullAndCows.printScore()
         // then, 예상 결과 확인
-        XCTAssertEqual(result, "S S S", "정답이 아닙니다")
+        XCTAssertEqual(result, "S S S", "정답이 잘못되었습니다")
     }
     
     // OOO일 경우
@@ -41,7 +49,7 @@ final class TDDTests: XCTestCase {
         // when, 테스트 코드 실행
         let result = bullAndCows.printScore()
         // then, 예상 결과 확인
-        XCTAssertEqual(result, "O O O", "정답이 아닙니다")
+        XCTAssertEqual(result, "O O O", "정답이 잘못되었습니다")
     }
     
     // BBB일 경우
@@ -52,7 +60,7 @@ final class TDDTests: XCTestCase {
         // when, 테스트 코드 실행
         let result = bullAndCows.printScore()
         // then, 예상 결과 확인
-        XCTAssertEqual(result, "B B B", "정답이 아닙니다")
+        XCTAssertEqual(result, "B B B", "정답이 잘못되었습니다")
     }
     
     // SOS일 경우
@@ -63,7 +71,7 @@ final class TDDTests: XCTestCase {
         // when, 테스트 코드 실행
         let result = bullAndCows.printScore()
         // then, 예상 결과 확인
-        XCTAssertEqual(result, "S O S", "정답이 아닙니다")
+        XCTAssertEqual(result, "S O S", "정답이 잘못되었습니다")
     }
 
     func testPerformanceExample() throws {
